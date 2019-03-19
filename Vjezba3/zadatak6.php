300c4b27836f095ca8ac8d9eb604b377cb35f2e5
Tino Andrijic <tino@andrijic.com>
2019-03-19 17:54:37 +0100

    zadatak 9

--- Vjezba3/zadatak6.php
+++ Vjezba3/zadatak6.php
@@ -5,34 +5,22 @@
     public function __construct($ime, $vrsta){
       $this->ime = $ime;
       $this->vrsta = $vrsta;
+      $this->Provjera();
       self::provjera();
       echo "<constructor<br />";
       echo 'Ime: '.$this->ime.' Vrsta: ', $vrsta.'<br />';
-      //$this->provjera();
+      $this->provjera();
     }
 
-    private function provjera(){
-      if ($vrsta != 'konj' || $vrsta != 'pas'){
+    private function Provjera(){
+      if ($this->vrsta != 'konj' && $this->vrsta != 'pas'){
         echo "<br />Pogreška! Životinja nije konj ili pas!<br";
         echo $ime, $vrsta.'<br />';
       }
     }
-
-      abstract function avgSpeed($vrijeme,$udaljenost);
   }
 
-class Zivotinja extends Animal
-{
-    function avgSpeed($vrijeme, $udaljenost){
 
 
-    }
-
-}
-
-$pas1 = new Zivotinja("Micko", "pas");
-$pas2 = new Zivotinja("Dogica", "pas");
-$macka1 = new Zivotinja("Musa", "macka");
-$konj1 = new Zivotinja("Spidko", "konj");
 
 ?>
---
+++ Vjezba3/zadatak7.php
@@ -0,0 +1,56 @@
+<?php
+
+  abstract class Animal {
+
+    public function __construct($ime, $vrsta){
+      $this->ime = $ime;
+      $this->vrsta = $vrsta;
+      $this->Provjera();
+      //self::provjera();
+      echo "<constructor Animal<br />";
+      echo 'Ime: '.$this->ime.' Vrsta: ', $vrsta.'<br />';
+
+    }
+
+    private function Provjera(){
+      if ($this->vrsta != 'konj' && $this->vrsta != 'pas'){
+        echo "<br />Pogreška! Životinja nije konj ili pas!<br";
+        echo $ime, $vrsta.'<br />';
+      }
+    }
+
+      abstract function avgSpeed($vrijeme,$udaljenost);
+  }
+
+class RaceAnimal extends Animal
+{
+  public static $najbrzaZivotinja = '';
+  public static $najbrziRezultat = 0;
+
+  public function __construct($ime, $vrsta, $vrijeme, $udaljenost){
+    parent:: __construct ($ime, $vrsta);
+    self ::avgSpeed($vrijeme, $udaljenost);
+    //echo "<constructor Zivotinja<br />";
+    //echo 'Ime: '.$this->ime.' Vrsta: ', $vrsta.'<br />';
+  }
+
+  public function avgSpeed($vrijeme, $udaljenost) {
+      $avgSpeed = round($udaljenost/$vrijeme,2);
+
+        if($avgSpeed > self:: $najbrziRezultat){
+          self::$najbrzaZivotinja = $this->ime;
+          self::$najbrziRezultat = $avgSpeed;
+        }
+      return $this->ime.': '.$avgSpeed.' km/h<br>';
+    }
+}
+
+$pas1 = new RaceAnimal("Micko", "pas", 80, 50);
+$pas2 = new RaceAnimal("Dogica", "pas", 20, 80);
+$macka1 = new RaceAnimal("Musa", "macka", 30, 100);
+$konj1 = new RaceAnimal("Spidko", "konj", 25,80);
+
+print 'najbrzi je <b>'.$pas1::$najbrzaZivotinja.'</b><br>';
+print 'rezultat: <b>'.$pas1::$najbrziRezultat.'</b>';
+
+?>
